2fa47162e981f1f3883397ea290ba416d3b3abd3
xiewenyan <1441950483@qq.com>
2024-09-02 11:49:46 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Description:
+ * @Author: XieWenyan
+ * @Date: 2024/9/2 11:48
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
